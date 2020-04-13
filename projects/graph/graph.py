bf44a054ad7de0cf351abe8b8fd57eb07091c5f2
Jason Loomis <jason@jrloomis.com>
2020-04-13 19:33:47 -0400

    depth first recursive

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -158,7 +158,7 @@ class Graph:
                     new_path.append(next_vert)
                     s.push(new_path)
 
-    def dfs_recursive(self, starting_vertex, destination_vertex):
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -166,7 +166,25 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+        if visited is None:
+            visited = set()
+
+        if path is None:
+            path = []
+
+        visited.add(starting_vertex)
+
+        path = path+[starting_vertex]
+
+        if starting_vertex == destination_vertex:
+            return path
+
+        for vert in self.vertices[starting_vertex]:
+            if vert not in visited:
+                new_path = self.dfs_recursive(
+                    vert, destination_vertex, visited, path)
+                if new_path:
+                    return new_path
 
 
 if __name__ == "__main__":
