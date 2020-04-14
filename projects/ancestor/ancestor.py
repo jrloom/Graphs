733e458f61c3fffaffb39fd19890ae326ac3359d
Jason Loomis <jason@jrloomis.com>
2020-04-14 18:23:05 -0400

    added graph

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -15,5 +15,24 @@ class Queue:
         else:
             return None
 
+class Graph:
+    def __init__(self):
+        self.verts = {}
+
+    def add_vert(self, vert_id):
+        self.verts[vert_id] = set()
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.verts and v2 in self.verts:
+            self.verts[v1].add(v2)
+        else:
+            print('error adding edge: verts not found')
+    
+    def get_neighbors(self, vert_id):
+        if vert_id in self.verts:
+            return self.verts[vert_id]
+        else:
+            return None
+
 def earliest_ancestor(ancestors, starting_node):
     pass
