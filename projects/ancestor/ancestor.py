b99699829a3f77e1c2a4a458b6a8351b19f6e9d1
Jason Loomis <jason@jrloomis.com>
2020-04-14 17:18:54 -0400

    made a queue

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,19 @@
 
+class Queue:
+    def __init__(self):
+        self.store = []
+
+    def size(self):
+        return len(self.store)
+
+    def enqueue(self, val):
+        self.store.append(val)
+    
+    def dequeue(self):
+        if self.size() > 0:
+            return self.store.pop(0)
+        else:
+            return None
+
 def earliest_ancestor(ancestors, starting_node):
     pass
